4d8dd44a7bf455d90066ec7f07d51dc8bd96f529
pedro011hrq <107210071+pedro011hrq@users.noreply.github.com>
2022-10-26 17:19:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//Crie o seguinte array: var deuses = ['Aegir', 'Aud', 'Balder']
+var deuses = ['Aegir', 'Aud', 'Balder'];
+console.log(deuses);
+deuses.push('Loki', 'Odin', 'Frey');
+console.log(deuses);
+
+
+//Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+ 
+let number = [5,7,1,8,9]
+number.sort()
+
+console.log(number)
+
+number.reverse()
+console.log(number)
+
+
